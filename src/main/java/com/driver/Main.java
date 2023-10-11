7d8bfd4e66172c456463c9d202c071d4d7027a2d
ss9324558 <ss9324558@gmail.com>
2023-10-12 01:27:04 +0530

    try1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A {
+
+    @Override
+    String meth() {
+        return "Method is overridden in Extendend class B";
+    }
+}
 public class Main {
-  
+    public static void main(String[] args) {
+        B b=new B();
+        System.out.println(b.meth());
+    }
 }
